4acdd386781f47cfe4b576785cb23f69c46af553
taikishank <shanktaiki@gmail.com>
2024-12-06 19:33:13 -0800

    Minor Fixes
    Updated ValueError message
    updated csv conversion code

--- dataset.py
+++ dataset.py
@@ -11,8 +11,6 @@ import os
 #   stores infromation into csv file
 #   csv files used in SQL database for easy querying
 
-
-
 def dataset():
 
 #   use BeautifulSoup to extract the main Big-5 table with all teams from top 5 leagues
@@ -29,14 +27,10 @@ def dataset():
 
 #   iterates through each team url and extracts the team name and leauge
 #   extracts respective data table and converts to csv 
-#   csv file dependent upon the league
+#   csv file directory dependent upon the league
 #   ex  Barcelona is in La_Liga, Tottenham is in Premier_League, etc...
 
-    counter = 0
-
     for link in team_urls:
-        # if (link == "https://fbref.com/en/squads/206d90db/Barcelona-Stats"):
-        #     continue
         team_name = link.split("/")[-1].replace("Stats", "").replace("-", " ")
         data = requests.get(link)
 
@@ -44,13 +38,13 @@ def dataset():
             players = pd.read_html(StringIO(data.text), match="Standard Stats")[0]
             players.columns = players.columns.droplevel()
         except ValueError:
-            print("Error trying to pass over table for " + team_name)
+            print("!!!CRUCIAL ERROR!!! Error trying to pass over table for " + team_name)
             pass
-
         
         players = players.loc[:, ~players.columns.duplicated()]
         player_data = players[['Player', 'Pos', 'Age', 'MP', 'Min', 'Gls', 'Ast', 'CrdY', 'CrdR', 'xG', 'xAG']]     #   relevant categories for data
         player_data = player_data[:-2]      #   last two rows are total team stats, not necessary for this program
+        team_name = team_name[:-1]
         player_data['Club'] = team_name
 
     #   many different instances of '/en/comps/', grab the second instance as it is easiest to extract league name
@@ -59,13 +53,10 @@ def dataset():
         soup = BeautifulSoup(requests.get(link).text, features="lxml")
         league = soup.find_all('a', href=lambda href: href and '/en/comps/' in href)[1]
         league = league.text.replace(' ', '_')
+        player_data['League'] = league
         player_data = pd.DataFrame(data = data)
         folder_path = r"D:\Taiki\Desktop\CS_Projects\fantasy\teamDatabase"
-        player_data.to_csv(folder_path + '\\' + league + '\\' + team_name, index=False)
+        player_data.to_csv(folder_path + '\\' + league + '\\' + team_name.replace(' ', "_") + '.csv', index=False)
 
         print(team_name + " successfully uploaded as csv to " + league)
         time.sleep(10)   # sleep so fbref gods dont ban
-
-        # if (counter == 4):
-        #     break
-        # counter += 1
